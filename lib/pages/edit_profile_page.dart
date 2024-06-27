f34a38b5ab1fc95fe665ec2892f0ba99a94aace2
Abdulrahmanaak <Abdulrahman1aak@gmail.com>
2024-06-27 10:36:55 +0300

    RADIO WIDGET

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -5,64 +5,103 @@ import 'package:flutter_test_application/components/user_Avatar.dart';
 import 'package:flutter_test_application/config/app_strings.dart';
 import 'package:flutter_test_application/styles/app_colors.dart';
 
-class EditProfilePage extends StatelessWidget {
-  const EditProfilePage({super.key});
+enum Gender {
+  none,
+  male,
+  female,
+}
+
+class EditProfilePage extends StatefulWidget {
+  EditProfilePage({super.key});
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  var gender = Gender.none;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Toolbar(title: AppStrings.editProfile),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: UserAvatar(
-                    size: 120,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: UserAvatar(
+                      size: 120,
+                    ),
                   ),
-                ),
-                Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: AppColors.primary,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(6),
+                  Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6),
+                            ),
                           ),
-                        ),
-                        child: Icon(
-                          Icons.edit,
-                          size: 20,
-                          color: Colors.black,
-                        ))),
-              ],
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            AppTextField(hint: AppStrings.firstName),
-            SizedBox(
-              height: 16,
-            ),
-            AppTextField(hint: AppStrings.lastName),
-            SizedBox(
-              height: 16,
-            ),
-            AppTextField(hint: AppStrings.phoneNumber),
-            SizedBox(
-              height: 16,
-            ),
-            AppTextField(hint: AppStrings.location),
-            SizedBox(
-              height: 16,
-            ),
-            AppTextField(hint: AppStrings.birthday),
-          ],
+                          child: Icon(
+                            Icons.edit,
+                            size: 20,
+                            color: Colors.black,
+                          ))),
+                ],
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              AppTextField(hint: AppStrings.firstName),
+              SizedBox(
+                height: 16,
+              ),
+              AppTextField(hint: AppStrings.lastName),
+              SizedBox(
+                height: 16,
+              ),
+              AppTextField(hint: AppStrings.phoneNumber),
+              SizedBox(
+                height: 16,
+              ),
+              AppTextField(hint: AppStrings.location),
+              SizedBox(
+                height: 16,
+              ),
+              AppTextField(hint: AppStrings.birthday),
+              Row(
+                children: [
+                  RadioListTile(
+                    title: Text(AppStrings.male),
+                    value: Gender.male,
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = Gender.male;
+                      });
+                    },
+                  ),
+                  RadioListTile(
+                    title: Text(AppStrings.female),
+                    value: Gender.female,
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = Gender.female;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
