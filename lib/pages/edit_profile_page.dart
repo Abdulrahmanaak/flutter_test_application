19ec1f6273a417d76ce5295d7381cb71b1cec893
Abdulrahmanaak <Abdulrahman1aak@gmail.com>
2024-06-27 10:18:43 +0300

    stack and positioned widget

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_application/components/app_text_field.dart';
 import 'package:flutter_test_application/components/toolbar.dart';
+import 'package:flutter_test_application/components/user_Avatar.dart';
 import 'package:flutter_test_application/config/app_strings.dart';
+import 'package:flutter_test_application/styles/app_colors.dart';
 
 class EditProfilePage extends StatelessWidget {
   const EditProfilePage({super.key});
@@ -14,6 +16,35 @@ class EditProfilePage extends StatelessWidget {
         padding: const EdgeInsets.all(24),
         child: Column(
           children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: UserAvatar(
+                    size: 120,
+                  ),
+                ),
+                Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: AppColors.primary,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.edit,
+                          size: 20,
+                          color: Colors.black,
+                        ))),
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
             AppTextField(hint: AppStrings.firstName),
             SizedBox(
               height: 16,
@@ -27,6 +58,10 @@ class EditProfilePage extends StatelessWidget {
               height: 16,
             ),
             AppTextField(hint: AppStrings.location),
+            SizedBox(
+              height: 16,
+            ),
+            AppTextField(hint: AppStrings.birthday),
           ],
         ),
       ),
